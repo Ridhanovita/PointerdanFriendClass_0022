b7c1a8b1a2211206d3c99c4f074ab39567c53ef6
Ridhanovita <novitaridha796@gmail.com>
2025-05-05 10:44:43 +0700

    pointer reference

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -9,3 +9,15 @@ class mahasiswa{
     }
 };
 
+int main(){
+    
+    mahasiswa mhs{1}; //Object mhs
+    mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
+
+  
+
+}
